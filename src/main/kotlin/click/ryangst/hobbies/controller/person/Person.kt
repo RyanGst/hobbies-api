28e5b104533877e656815552aa607c2b934f676c
ryangst <ryangst.hire@gmail.com>
2024-06-29 12:15:43 -0300

    annotate person as Entity

--- src/main/kotlin/click/ryangst/hobbies/controller/person/Person.kt
+++ src/main/kotlin/click/ryangst/hobbies/controller/person/Person.kt
@@ -1,9 +1,19 @@
 package click.ryangst.hobbies.controller.person
 
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "person")
 data class Person(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long = 0,
+    @Column(name = "first_name", nullable = false)
     var firstName: String = "",
+    @Column(name = "last_name", nullable = false)
     var lastName: String = "",
+    @Column
     var address: String = "",
-    var gender : String = "",
+    @Column
+    var gender: String = "",
 )
