7f6735e6fbebe4b57ba435853ad0377c3865877f
ryangst <ryangst.hire@gmail.com>
2024-07-05 18:27:22 -0300

    fix field name on PersonVO

--- src/main/kotlin/click/ryangst/hobbies/data/vo/v1/PersonVO.kt
+++ src/main/kotlin/click/ryangst/hobbies/data/vo/v1/PersonVO.kt
@@ -1,11 +1,13 @@
 package click.ryangst.hobbies.data.vo.v1
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.github.dozermapper.core.Mapping
 import org.springframework.hateoas.RepresentationModel
 
 
 data class PersonVO(
     @Mapping("id")
+    @field:JsonProperty("id")
     var key: Long = 0,
     var firstName: String = "",
     var lastName: String = "",
