90d8ca3ca82273d347ec9065c7611b973930d945
ryangst <ryangst.hire@gmail.com>
2024-07-06 12:50:19 -0300

    add many to many relation on User

--- src/main/kotlin/click/ryangst/hobbies/modules/user/User.kt
+++ src/main/kotlin/click/ryangst/hobbies/modules/user/User.kt
@@ -1,33 +1,56 @@
 package click.ryangst.hobbies.modules.user
 
+import click.ryangst.hobbies.modules.permission.Permission
 import jakarta.persistence.*
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
 
 @Entity
 @Table(name = "users", uniqueConstraints = [UniqueConstraint(columnNames = ["user_name"])])
-data class User(
+class User : UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
-    val id: Long = 0,
+    val id: Long = 0
 
     @Column(name = "user_name", nullable = true, length = 255)
-    val userName: String? = null,
+    val userName: String? = null
 
     @Column(name = "full_name", nullable = true, length = 255)
-    val fullName: String? = null,
+    val fullName: String? = null
 
     @Column(name = "password", nullable = true, length = 255)
-    val password: String? = null,
+    private val password: String? = null
 
     @Column(name = "account_non_expired", nullable = true)
-    val accountNonExpired: Boolean? = null,
+    val accountNonExpired: Boolean? = null
 
     @Column(name = "account_non_locked", nullable = true)
-    val accountNonLocked: Boolean? = null,
+    val accountNonLocked: Boolean? = null
 
     @Column(name = "credentials_non_expired", nullable = true)
-    val credentialsNonExpired: Boolean? = null,
+    val credentialsNonExpired: Boolean? = null
 
     @Column(name = "enabled", nullable = true)
     val enabled: Boolean? = null
-)
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+        name = "user_permission",
+        joinColumns = [JoinColumn(name = "id_user")],
+        inverseJoinColumns = [JoinColumn(name = "id_permission")]
+    )
+    var permissions = mutableListOf<Permission>()
+
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
+        return mutableListOf()
+    }
+
+    override fun getPassword(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun getUsername(): String {
+        TODO("Not yet implemented")
+    }
+}
