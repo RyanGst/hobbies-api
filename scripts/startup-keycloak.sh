dd41915e104675802344cd26ffac8a811c8f759a
ryangst <ryangst.hire@gmail.com>
2024-08-09 15:57:35 -0300

    add ssh into server config to github-action

--- scripts/startup-keycloak.sh
+++ scripts/startup-keycloak.sh
@@ -1,1 +1,3 @@
 docker run -p 8080:8080 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin quay.io/keycloak/keycloak:25.0.1 start-dev
+
+# scp -r /Users/ryan/IdeaProjects/hobbies/docker-compose.yml /Users/ryan/IdeaProjects/hobbies/Dockerfile /Users/ryan/IdeaProjects/hobbies/nginx/ root@142.93.10.34:/root/hobbies
